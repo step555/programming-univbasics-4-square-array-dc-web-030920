03b10690e52d675cbe1896efffe14cbbac98b6f3
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-25 03:50:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,17 +1,17 @@
-# def square_array(array)
-#   counter = 0
-#   while array[counter] do
-#     array[counter]**
-#     counter +=1
-#   end
-# end
-
 def square_array(array)
-  newArray = []
   counter = 0
-  while counter < array.length do
-    newArray << array[counter] ** 2
-    counter += 1
+  while array[counter] do
+    array[counter]**
+    counter +=1
   end
-  newArray
 end
+
+# def square_array(array)
+#   newArray = []
+#   counter = 0
+#   while counter < array.length do
+#     newArray << array[counter] ** 2
+#     counter += 1
+#   end
+#   newArray
+# end
